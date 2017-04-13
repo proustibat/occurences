432e58e02680345562ed15b6a38091a4ad64384e
Proustibat <jennifer.proust@gmail.com>
2017-04-13 22:23:39 +0200

    feat($console): First commit for a simple research of occurences in a text

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const mystring = "Not connected to power";
+const lookingFor = "power";
+let occurences = 0;
+let myStrToArr = mystring.split(" ");
+myStrToArr.forEach(function(word) {
+  if(word.length > 2 && lookingFor === word) {
+    occurences++;
+  }
+});
+
+// TODO : gestion ponctuation (surtout pour l'anglais où il n'y a pas d'espace avant les deux points par exemple)
+// TODO : gestion majuscule/minuscule
+// TODO : dynamisation du texte de base et dynamisation du texte a chercher
+// TODO : voir si avec la methode replace on peut pas avoir le nombre d'occurences directements
+
+
+console.log("------------------------");
+console.log("Le texte de base : ");
+console.log(mystring);
+console.log("------------");
+console.log("Le mot recherché : ");
+console.log(lookingFor);
+console.log("------------");
+console.log("Nombre d'occurences: ", occurences);
+console.log("------------------------");
