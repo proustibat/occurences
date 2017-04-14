87eeaff516b48b57f9188fee3241349b78450f75
Jennifer Proust <jennifer.proust@gmail.com>
2017-04-14 14:41:59 +0200

    refactor: Transform into a npm module
    Add a license, add testing, add package description, add commitizen
    
    BREAKING CHANGE: Module is now an exported class

--- js/main.js
+++ js/main.js
@@ -1,62 +1,67 @@
 const request = require("request");
-const url = "http://faker.hook.io/?property=lorem.paragraphs";
+const url = "http://faker.hook.io/?property=lorem.sentences";
 
 /**
  * COMPTER LE NOMBRE D'OCCURENCES DE CHAQUE MOT DANS UN TEXTE
  */
-const countOccurences = (text) => {
+class Occurrences {
 
-    // Retire les ponctuations
-    const myCleanedText = text.replace(/[^A-Za-z0-9_]/g," ");
+    constructor(text) {
+        // Retire les ponctuations
+        const myCleanedText = text.replace(/[^A-Za-z0-9_]/g," ");
 
-    // Split le texte en un tableau de mots
-    const wordsArray = myCleanedText.split(" ");
+        // Split le texte en un tableau de mots
+        const wordsArray = myCleanedText.split(" ");
 
-    // Contiendra chaque mot et son nombre d'occurences
-    let result = {};
+        // Contiendra chaque mot et son nombre d'occurences
+        let result = {};
 
-    // Parse chaque item du tableau
-    wordsArray.forEach(function(word){
+        // Parse chaque item du tableau
+        wordsArray.forEach(function(word){
 
-        // Supprime le cas de majuscule
-        word = word.toLowerCase();
+            // Supprime le cas de majuscule
+            word = word.toLowerCase();
 
-        // Verifie longueur du mot
-        if(word.length > 2) {
+            // Verifie longueur du mot
+            if(word.length > 2) {
 
-            // premiere fois qu'on rencontre le mot
-            if(!result[word]) {
-                result[word] = 1;
+                // premiere fois qu'on rencontre le mot
+                if(!result[word]) {
+                    result[word] = 1;
+                }
+                else {
+                    // mot deja rencontre, on incremente son nombre
+                    result[word]++;
+                }
             }
-            else {
-                // mot deja rencontre, on incremente son nombre
-                result[word]++;
-            }
-        }
-    });
+        });
 
-    console.log("------------------------");
-    console.log("Nombre d'occurences de chaque mot:");
-    console.log(result);
-    console.log("------------------------");
-
+        return result;
+    }
 };
 
-// const myText = "Not connected to power. Power is it good or bad. What is power? Dunno what power is but I know what it's not.";
-console.log("------------------------");
-console.log("Requests a fake text at ", url, "...");
-console.log("------------------------");
-request({
-    url: url,
-    json: true
-}, function (error, response, data) {
-    if (!error && response.statusCode === 200) {
-        countOccurences(data);
-    }
-    else {
-        console.log("It seems an error occured when requesting ", url);
-    }
-});
+module.exports = Occurrences;
+
+// // // const myText = "Not connected to power. Power is it good or bad. What is power? Dunno what power is but I know what it's not.";
+// console.log("-----------------------------");
+// console.log("Requests a fake text at ", url, "...");
+// console.log("-----------------------------");
+// request({
+//     url: url,
+//     json: true
+// }, function (error, response, data) {
+//     if (!error && response.statusCode === 200) {
+//         let myResult = new Occurrences(data);
+//         console.log("------------------------");
+//         console.log("Nombre d'occurrences de chaque mot:");
+//         console.log(myResult);
+//         console.log("------------------------");
+//
+//     }
+//     else {
+//         console.log("It seems an error occured when requesting ", url);
+//     }
+// });
 
 
 
---
+++ test/test.js
@@ -0,0 +1,24 @@
+// Import chai.
+let chai = require('chai'),
+    path = require('path');
+let should = chai.should,
+    expect = chai.expect;
+
+// Tell chai that we'll be using the "expect" style assertions.
+chai.expect();
+
+// Import the Occurences class.
+let Occurrences = require(path.join(__dirname, '..', 'js', 'main'));
+
+describe('Occurrences', () => {
+    let occurrences;
+
+    beforeEach(() => {
+        // Create a new instance object before every test.
+        occurrences = new Occurrences('bla bli blou');
+    });
+
+    it('returns an object', () => {
+        expect(occurrences).to.be.an('object');
+    });
+});
